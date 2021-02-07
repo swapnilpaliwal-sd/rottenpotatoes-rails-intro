e66adbeeedbdf30e614b231366ceb560c501b322
SWAPNIL PALIWAL <sp3911@columbia.edu>
2021-02-07 03:08:22 +0000

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ActiveRecord::Base
   def self.all_ratings
-    x = self.select(:rating).distinct.order(:rating)
-    return x.pluck(:rating)
+    x = self.select(:rating)
+    x = x.distinct #ensure distinct entries
+    return x.order(:rating).pluck(:rating)
   end
 end
